4b80e855a526fbe4b9e7d6595ca88f655c4c87ce
Sovika Poudel <sovikapoudel@gmail.com>
2021-04-06 01:49:12 -0500

    Added BMI and BFP Calculator
    Body fat percentage calculator added to the webpage.
    When the BMI is calculated, it will automatically gets printed in Body fat percentage BMI parts: where id is used to reference the value.
    added some JS code for Body Fat Percentage Calculator.
    CSS for table and it's properties are added
    BFP calculation for male and female need to be added.

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
-alert("Please Click 'OK' to continue");
-{/* <form name="bmiForm">
-        Your Weight(kg): <input type="text" name="weight" size="10"><br />
-        Your Height(cm): <input type="text" name="height" size="10"><br />
-        <input type="button" value="Calculate BMI" onClick="calculateBmi()"><br />
-        Your BMI: <input type="text" name="bmi" size="10"><br />
-        This Means: <input type="text" name="meaning" size="25"><br />
-        <input type="reset" value="Reset" />
-        </form>
-    <script src="index.js"></script>
-    <script language="JavaScript">
-        function calculateBmi() { */}
-        function calculateBMI(){
+// alert("Please Click 'OK' to continue");
 
-        }
-        var weight = document.bmi.weight.value
-        var height = document.bmi.height.value
-        if(weight > 0 && height > 0){	
-        var finalBmi = weight/(height/100*height/100)
-        document.bmi.bmi.value = finalBmi
-        if(finalBmi < 18.5){
-        document.bmi.meaning.value = "That you are too thin."
-        }
-        if(finalBmi > 18.5 && finalBmi < 25){
-        document.bmi.meaning.value = "That you are healthy."
-        }
-        if(finalBmi > 25){
-        document.bmi.meaning.value = "That you have overweight."
-        }
-        }
-        else{
-        alert("Please Fill in everything correctly")
-        }
-        
-        // //-->
-        // </script>
+            var finalBmi
+            
+            function CalculateBmi() {
+                var weight = document.bmicalculator.weight.value
+                var height = document.bmicalculator.height.value
+                if(weight > 0 && height > 0){	
+                    finalBmi = weight/(height/100*height/100)
+                    document.getElementById("bfp-bmi").value =finalBmi
+                    document.bmicalculator.bmi.value = finalBmi
+                    if(finalBmi < 18.5){
+                        document.bmicalculator.meaning.value = "That you are too thin."
+                    }
+                    if(finalBmi > 18.5 && finalBmi < 25){
+                        document.bmicalculator.meaning.value = "That you are healthy."
+                    }
+                    if(finalBmi > 25){
+                        document.bmicalculator.meaning.value = "That you have overweight."
+                    }
+                }
+                else{
+                    alert("Please provide the correct information!");
+                }
+            }
+
+            function CalculateBodyFatPercentage(){
+                var age = document.bfpcalculator.age.value
+                //  var gender = document.bfpcalculator.gender.value
+                if (age>0 ); 
+                // && gender==male or female (need to add this part)
+                    finalbfp = 1.20*finalBmi+(0.23*age)-16.2
+                    // this calculation is for male
+                    document.bfpcalculator.bfp.value = finalbfp
+
+                        if( finalbfp > 8 && finalbfp < 19 ){
+                            document.bfpcalculator.meaning.value = "Your body fat percentage is too high "
+                        }
+                        if( finalbfp > 19){
+                            document.bfpcalculator.meaning.value = "Your body fat percentage is too high "
+                        }
+
+                else{
+                            alert("Please provide the correct information!");
+                        }
+
+                
+                    
+
+            }
+
+            
+            var waist = document.getElementById("waist");
+            var height = document.getElementById("height");
+            var waist = document.getElementById("waist");
+            var age = document.getElementById("age");
+           
